11e61ebd61db3e8a8ac3702c72dcf03b90011f8b
andig <cpuidle@gmx.de>
2022-08-01 22:35:35 +0200

    Ocpp: use builtin timeout

--- charger/ocpp/instance.go
+++ charger/ocpp/instance.go
@@ -4,7 +4,15 @@ import (
 	"time"
 
 	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/request"
+	"github.com/lorenzodonini/ocpp-go/ocpp"
 	ocpp16 "github.com/lorenzodonini/ocpp-go/ocpp1.6"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/core"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/firmware"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/localauth"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/remotetrigger"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/reservation"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/smartcharging"
 	"github.com/lorenzodonini/ocpp-go/ocppj"
 )
 
@@ -12,7 +20,13 @@ var instance *CS
 
 func Instance() *CS {
 	if instance == nil {
-		cs := ocpp16.NewCentralSystem(nil, nil)
+		dispatcher := ocppj.NewDefaultServerDispatcher(ocppj.NewFIFOQueueMap(0))
+		dispatcher.SetTimeout(request.Timeout)
+
+		profiles := []*ocpp.Profile{core.Profile, localauth.Profile, firmware.Profile, reservation.Profile, remotetrigger.Profile, smartcharging.Profile}
+		endpoint := ocppj.NewServer(nil, dispatcher, nil, profiles...)
+
+		cs := ocpp16.NewCentralSystem(endpoint, nil)
 
 		instance = &CS{
 			log:           util.NewLogger("ocpp"),
