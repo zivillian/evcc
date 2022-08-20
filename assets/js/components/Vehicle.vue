6d464b6f98642793c70b0649ff07aab63c2714c7
andig <cpuidle@gmx.de>
2022-08-20 12:21:14 +0200

    Recursive collector

--- assets/js/components/Vehicle.vue
+++ assets/js/components/Vehicle.vue
@@ -70,7 +70,7 @@ export default {
 		enabled: Boolean,
 		charging: Boolean,
 		minSoC: Number,
-		vehicleDetectionActive: Boolean,
+		// vehicleDetectionActive: Boolean,
 		vehicleRange: Number,
 		vehicleTitle: String,
 		targetTimeActive: Boolean,
--- assets/js/mixins/collector.js
+++ assets/js/mixins/collector.js
@@ -2,6 +2,16 @@ export default {
   methods: {
     // collect all target component properties from current instance
     collectProps: function (component) {
+      let data = this.collectComponentProps(component);
+      for (var c in component.components) {
+        console.log(c);
+        console.log(component.components[c]);
+        Object.assign(data, this.collectProps(component.components[c]));
+      }
+      console.log(data);
+      return data;
+    },
+    collectComponentProps: function (component) {
       let data = {};
       for (var p in component.props) {
         if (p in this) {
